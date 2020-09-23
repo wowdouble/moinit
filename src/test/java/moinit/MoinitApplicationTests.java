d0a01d8c950aaf2f10f62eb526f6120bc68b7d07
liudashuang <liudashuang@superatomfin.com>
2020-09-23 20:04:02 +0800

    Initial commit

---
+++ src/test/java/moinit/MoinitApplicationTests.java
@@ -0,0 +1,13 @@
+package moinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoinitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
